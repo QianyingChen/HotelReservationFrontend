d0c88c77ae3fe9c71390e60e5b46a89199804d1a
Teresa Chen <qianyingteresachen@gmail.com>
2023-05-23 22:58:45 -0400

    added sign up page, and dirct to user page if sign up successful

--- HotelReservationFrontend/src/App.tsx
+++ HotelReservationFrontend/src/App.tsx
@@ -5,6 +5,7 @@ import HotelList from './pages/hotellist/HotelList'
 import Hotel from './pages/hotel/Hotel'
 import User from './pages/user/User'
 import Navbar from './components/Navbar'
+import SignUpPage from './pages/user/SignUpPage'
 
 function App() {
  
@@ -19,6 +20,7 @@ function App() {
       <Route path='/hotels/:id/rooms' element={<Hotel/>}/>
       <Route path='/users/:id' element={<User/>}/>
       <Route path='/rooms/:id/reserve' element={<User/>}/>
+      <Route  path="/users/signup" element={<SignUpPage/>} />
       </Routes>
 
     </BrowserRouter>
---
+++ HotelReservationFrontend/src/pages/user/SignUpPage.tsx
@@ -0,0 +1,157 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Button, Card, CardContent, Grid, Snackbar, TextField } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useCreateUserMutation } from '../../api/userApi';
+
+type SignUpFormData = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  email: string;
+  phoneNumber: string;
+};
+
+const SignUpForm = () => {
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const { register, handleSubmit, formState: { errors } } = useForm<SignUpFormData>();
+  const [createUser, { isError }] = useCreateUserMutation();
+  const navigate = useNavigate();
+
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      const response = await createUser({
+        firstName: data.firstName,
+        lastName: data.lastName,
+        username: data.username,
+        password: data.password,
+        email: data.email,
+        phoneNumber: data.phoneNumber,
+      });
+
+      if ('data' in response) {
+        setSnackbarMessage('User signed up successfully!');
+        setOpenSnackbar(true);
+        navigate('/user'); // Redirect to the user page
+      } else {
+        throw new Error('Failed to sign up.');
+      }
+    } catch (error: any) {
+      if (error.response?.data?.message === 'Username is already taken') {
+        setSnackbarMessage('User name already taken');
+      } else {
+        setSnackbarMessage('Failed to sign up. Please try again later.');
+      }
+      setOpenSnackbar(true);
+    }
+  };
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '88vh' }}>
+      <Card style={{ width: '300px' }}>
+        <CardContent>
+          <h1 style={{ textAlign: 'center' }}>Sign Up Page</h1>
+          <form onSubmit={handleSubmit(onSubmit)} noValidate>
+            <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <TextField
+                  {...register('firstName', {
+                    required: 'First name is required',
+                  })}
+                  variant="outlined"
+                  fullWidth
+                  label="First Name"
+                  autoFocus
+                  error={!!errors.firstName}
+                  helperText={errors.firstName?.message}
+                />
+              </Grid>
+              <Grid item xs={12} >
+                <TextField
+                  {...register('lastName', {
+                    required: 'Last name is required',
+                  })}
+                  variant="outlined"
+                  fullWidth
+                  label="Last Name"
+                  error={!!errors.lastName}
+                  helperText={errors.lastName?.message}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  {...register('username', {
+                    required: 'Username is required',
+                  })}
+                  variant="outlined"
+                  fullWidth
+                  label="Username"
+                  error={!!errors.username}
+                  helperText={errors.username?.message}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  {...register('password', {
+                    required: 'Password is required',
+                  })}
+                  variant="outlined"
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  error={!!errors.password}
+                  helperText={errors.password?.message}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  {...register('email', {
+                    required: 'Email is required',
+                  })}
+                  variant="outlined"
+                  fullWidth
+                  label="Email"
+                  type="email"
+                  error={!!errors.email}
+                  helperText={errors.email?.message}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  {...register('phoneNumber', {
+                    required: 'Phone number is required',
+                  })}
+                  variant="outlined"
+                  fullWidth
+                  label="Phone Number"
+                  error={!!errors.phoneNumber}
+                  helperText={errors.phoneNumber?.message}
+                />
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained" type="submit" fullWidth>
+                Submit
+              </Button>
+            </Grid>
+          </form>
+        </CardContent>
+      </Card>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={3000}
+        onClose={handleCloseSnackbar}
+        message={snackbarMessage}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      />
+    </div>
+  );
+};
+
+export default SignUpForm;
